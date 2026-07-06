bf6f7b28a6649372bcc2aa212446feb5e46f9e6c
panghwi <kanean99@naver.com>
2026-07-06 21:50:55 +0900

    initial commit

---
+++ src/main/java/com/kwanghwi/lastmessage/LastMessageApplication.java
@@ -0,0 +1,13 @@
+package com.kwanghwi.lastmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastMessageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kwanghwi/lastmessage/LastMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwanghwi.lastmessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastMessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
