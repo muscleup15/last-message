79c2e05f68bf017d14b99ee76aa8154f6e2a598a
panghwi <kanean99@naver.com>
2026-07-11 22:35:28 +0900

    add messageRepository

---
+++ src/main/java/com/kwanghwi/lastmessage/message/repository/MessageRepository.java
@@ -0,0 +1,12 @@
+package com.kwanghwi.lastmessage.message.repository;
+
+import com.kwanghwi.lastmessage.message.domain.Message;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface MessageRepository  extends ReactiveCrudRepository<Message, Long> {
+    Flux<Message> findByReceiverPhone(String receiverPhone);
+    
+
+}
