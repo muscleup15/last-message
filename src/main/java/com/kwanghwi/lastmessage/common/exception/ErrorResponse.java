8182e1f00f84ea5a1c279afebfd072149c9aaf2e
panghwi <kanean99@naver.com>
2026-07-28 17:49:00 +0900

    add getter for jackson

--- src/main/java/com/kwanghwi/lastmessage/common/exception/ErrorResponse.java
+++ src/main/java/com/kwanghwi/lastmessage/common/exception/ErrorResponse.java
@@ -1,10 +1,12 @@
 package com.kwanghwi.lastmessage.common.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 public class ErrorResponse {
     private String code;
     private String message;
